3f8dc48e16cdd440b4779e7b39599c3724471d44
DENNISDGR <88437053+DENNISDGR@users.noreply.github.com>
2022-04-20 19:43:42 +0300

    Delete website.js

--- commands/website.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    name: 'website',
-    permissions: [],
-    cooldown: 5,
-    description: "this is a website command",
-    execute(message, args, cmd, client, Discord){
-        message.channel.send('https://imtheghost6321.wixsite.com/my-site-5');
-    }
-}
